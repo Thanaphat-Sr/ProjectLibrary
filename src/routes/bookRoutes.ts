dbc1245d3a5f417ded50a3674867a8c0c1ca66a7
kittanai <kittanai_yam@cmu.ac.th>
2025-03-01 14:35:50 +0700

    Add Try, Catch, Finally to bookRoute

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -10,15 +10,26 @@ router.get("/", async(req, res) => {
         const pageSize = parseInt(req.query.pageSize.toString()) || 3;
         const pageNo = parseInt(req.query.pageNo.toString()) || 1;
         const keyword = req.query.keyword as string;
-        const result = await service.getAllBooksWithAuthorPagination(keyword, pageSize, pageNo);
         
-        if (result.books.length === 0) {
-            res.status(404).send("No books found");
+        try{
+            const result = await service.getAllBooksWithAuthorPagination(keyword, pageSize, pageNo);
+        
+            if (result.books.length === 0) {
+                res.status(404).send("No books found");
+                return;
+            }
+            res.setHeader("x-total-count", result.count.toString());
+            res.json(result.books);
+        } catch (error) {
+            if (pageNo < 1 || pageSize < 1) {
+                res.status(400).send("Invalid pageNo or pageSize");
+            } else {
+                res.status(500).send("Internal Server Error");    
+            }
             return;
+        } finally {
+            console.log(`Request is completed. with pageNo = ${pageNo} and pageSIze=${pageSize}`)
         }
-
-        res.setHeader("x-total-count", result.count.toString());
-        res.json(result.books);
     } else if (req.query.title) {
         const title = req.query.title.toString().toLowerCase();
         const filteredBooks = await service.getBooksByTitle(title);
